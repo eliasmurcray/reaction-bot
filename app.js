418a6ae4991785e829cde4f67a889ac3ab67fb15
Ubuntu <notpolar@ReactionBot.ahr1g5v4rbwexnc02nvrb52cmd.bx.internal.cloudapp.net>
2023-03-13 06:50:21 +0000

    Update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const { Client, Events, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
 const { token, clientId } = require('./secret.json');
-const { execute } = require('./commands/reactrole');
 const commandFiles = fs.readdirSync('commands');
 const commands = [];
 commandFiles.forEach((file) => {
@@ -23,56 +22,55 @@ refreshCommands(commands);
 // Initialize bot
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, c => {
-  console.log('Ready! Logged in as ' + c.user.tag);
+client.once(Events.ClientReady, (bot) => {
+  console.log('Ready! Logged in as ' + bot.user.tag);
+
+	bot.on(Events.InteractionCreate, async interaction => {
+		if (!interaction.isChatInputCommand()) return;
+	
+		const command = client.commands.get(interaction.commandName);
+	
+		if (!command) return;
+	
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+	});
+	
+	bot.on(Events.MessageReactionAdd, async (reaction, user) => {
+		console.log(reaction, user);
+		const data = JSON.parse(fs.readFileSync('./database/data.json'));
+		const message = data.messages[reaction.message.id];
+		console.log(message);
+		if(message) {
+			const emoji = reaction._emoji.name;
+			console.log(emoji);
+			if(message.emojis[emoji]) {
+				const role = reaction.message.guild.roles.cache.get(message.emojis[emoji]);
+				console.log(role);
+				if (!role) return;
+				const member = await reaction.message.guild.members.cache.get(user.id);
+				console.log(member);
+				member.roles.add(role);
+				console.log(`Role '${role}' added to ${user.tag}.`);
+			} else {
+				reaction.remove();
+			}
+		}
+	});
 });
 
 // Commands
 client.commands = new Collection();
 commands.forEach((command) => {
   client.commands.set(command.data.name, command);
-});
-
-// Events
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-client.on(Events.MessageReactionAdd, async (reaction, user) => {
-	console.log(reaction, user);
-	const data = JSON.parse(fs.readFileSync('./database/data.json'));
-	const message = data.messages[reaction.message.id];
-	console.log(message);
-	if(message) {
-		const emoji = reaction._emoji.name;
-		console.log(emoji);
-		if(message.emojis[emoji]) {
-			const role = reaction.message.guild.roles.cache.get(message.emojis[emoji]);
-			console.log(role);
-			if (!role) return;
-			const member = await reaction.message.guild.member.cache.get(user.id);
-			console.log(member);
-			member.roles.add(role);
-			console.log(`Role '${role}' added to ${user.tag}.`);
-		} else {
-			reaction.remove();
-		}
-	}
 });
 
 client.login(token);
